82a7bc754439b89c358ec673f075d4be893499cd
guywithoutlif3 <72743368+guywithoutlif3@users.noreply.github.com>
2021-12-16 15:19:01 +0100

    Listing of Tests
    List of all things needed to be tested

--- src/main/test/java/test.java
+++ src/main/test/java/test.java
@@ -1,2 +1,26 @@
 public class test {
+
+    /* Needs testing:
+    *       Command Klasse
+    * -Command konstruktoren
+    * -getCommandWord + getSecondWord
+    * -isUnknown + hasSecondWord
+    *       CommandWords
+    * -isCommand
+    * -showAll
+    *       Game
+    * -play
+    *-processCommand
+    *-goRoom
+    *       Parser
+    * -KonstrukorParser
+    *-getCommand
+    *-showCommands
+    *       Room
+    *-Konstruktor Room
+    *-setExits
+    *-shortDescription
+    *-longDescription
+    *-exitString
+n    *-extRoom*/
 }
