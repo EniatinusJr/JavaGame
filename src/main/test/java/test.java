e426f75bebbfc61c438c6166e6458219585b77ba
guywithoutlif3 <72743368+guywithoutlif3@users.noreply.github.com>
2021-12-16 16:22:11 +0100

    did few Tests

--- src/main/test/java/test.java
+++ src/main/test/java/test.java
@@ -1,11 +1,31 @@
+import ch.bbw.zork.Command;
+import static  org.junit.Assert.*;
+import org.junit.Test;
 public class test {
 
     /* Needs testing:
     *       Command Klasse
-    * -Command konstruktoren
-    * -getCommandWord + getSecondWord
     * -isUnknown + hasSecondWord
-    *       CommandWords
+    *       CommandWords */
+
+@Test public void testCommandKonstruktor1() {
+    Command testCommand = new Command("test");
+    assert ("test".equals(testCommand.getCommandWord()));
+}
+@Test public void testCommandKonstruktor2() {
+    Command testCommand = new Command("test","test2");
+    assert ("test".equals(testCommand.getCommandWord()) && "test2".equals(testCommand.getSecondWord()));
+}
+@Test public void testIsUnkown(){
+    Command testCommand = new Command("help");
+    assertEquals(true, testCommand.isUnknown());
+    Command testCommand1 = new Command("notValid");
+    assertEquals(false,testCommand1.isUnknown());
+}
+
+
+
+    /*
     * -isCommand
     * -showAll
     *       Game
